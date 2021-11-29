acdb5a8aa03591b3e2a80b97f805f082bb7fe5a7
GritDim <gritsun.d@gmail.com>
2021-11-30 02:40:28 +0300

    Добавьте файлы проекта.

---
+++ Task6.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите предложение");
+            string startString = Console.ReadLine();
+
+            string[] stringArray = startString.Split();
+
+            foreach (string s in stringArray)
+            {
+                Console.WriteLine("{0}", s);
+            }
+
+            int maxLenght = 0;
+            string maxWord = "";
+
+            foreach (string s in stringArray)
+            {
+                if (s.Length > maxLenght)
+                {
+                    maxLenght = s.Length;
+                    maxWord = s;
+                }
+            }
+
+            
+
+
+            Console.WriteLine("Самое динное слово в предложении - {0} из {1} букв", maxWord, maxLenght);
+
+            Console.WriteLine("Нахмите любую клавишу");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task6.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string startString = Console.ReadLine();
+
+            string[] stringArray = startString.Split();
+
+            
+            string joinedString = String.Join("",stringArray);
+
+            joinedString = joinedString.ToLower();
+            Console.WriteLine("{0}", joinedString); //выводим изначальную строку без пробелов и все буквы в нижнем регистре
+            int sLenght = joinedString.Length;
+
+            string reverseString = "";       
+
+            for (int i = (sLenght-1); i >= 0; i--)
+            {
+                reverseString += joinedString.Substring(i, 1);
+            }
+
+            Console.WriteLine("{0}", reverseString); //выводим реверс строку без пробелов и все буквы в нижнем регистре
+            Console.WriteLine();
+
+            int proverka = String.Compare(reverseString,joinedString);
+
+            if (proverka == 0)
+            {
+                Console.WriteLine("Исходное предложение - палиндром!");
+            }
+            else
+            {
+                Console.WriteLine("Предложение не палиндром.");
+            }
+
+            Console.WriteLine("Нахмите любую клавишу");
+            Console.ReadKey();
+
+        }
+    }
+}
